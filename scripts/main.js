c5eca538895093ff52f847286f0721578a43fc66
lovisaspangberg <lovisaspangberg@gmail.com>
2017-08-14 19:15:52 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,74 +1,71 @@
+var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+if (width > 500) {
+  $(window).scroll(function(){
+    if ( $(this).scrollTop() > 0) {
+        $('.landing-headings').fadeOut();
+      } else {
+        $('.landing-headings').fadeIn();
+      }
+  });
+}
+
 $(document).ready(function(){
-
-
-
+  function draw() {
+    requestAnimationFrame(draw);
+    // Drawing code goes here
+    scrollEvent();
+  }
+  draw();
 });
-$(window).scroll(function(){
-  if($(this).scrollTop()>0)
-    {
-      $('.landing-headings').fadeOut();
-    }
-    else{
-      $('.landing-headings').fadeIn();
-    }
-  });
 
+function scrollEvent(){
+  if(!is_touch_device()){
+    viewportTop = $(window).scrollTop();
+    windowHeight = $(window).height();
+    viewportBottom = windowHeight + viewportTop;
+    if($(window).width())
 
-  $(document).ready(function(){
-
-      function draw() {
-          requestAnimationFrame(draw);
-          // Drawing code goes here
-          scrollEvent();
+    $('.about__row').each(function(){
+      var fadeBuffer = 200
+      var elementRect = this.getBoundingClientRect()
+      var elementHeight = elementRect.height
+      var elementBottom = elementRect.bottom
+      var elementDistanceFromBottom = windowHeight - elementBottom + elementHeight - fadeBuffer
+      if (elementDistanceFromBottom > 0) {
+        this.classList.add('about__row--expanded')
       }
-      draw();
-
-  });
-
-  function scrollEvent(){
-
-      if(!is_touch_device()){
-          viewportTop = $(window).scrollTop();
-          windowHeight = $(window).height();
-          viewportBottom = windowHeight + viewportTop;
-
-          if($(window).width())
-
-
-
-          $('.about__row').each(function(){
-              var fadeBuffer = 200
-              var elementRect = this.getBoundingClientRect()
-              var elementHeight = elementRect.height
-              var elementBottom = elementRect.bottom
-              var elementDistanceFromBottom = windowHeight - elementBottom + elementHeight - fadeBuffer
-              if (elementDistanceFromBottom > 0) {
-                this.classList.add('about__row--expanded')
-              }
-//              var scale = elementDistanceFromBottom / elementHeight
-
-//              if (scale < 0) {
-//                $(this).css({'transform':'scale(0)','opacity':'0'});
-//              } else if (scale > 1) {
-//                $(this).css({'transform':'scale(1)','opacity':'1'});
-//              } else {
-//                $(this).css({'transform':'scale(' + scale + ')','opacity':scale});
-//              }
-          });
-
-      }
-  }
-
-  function is_touch_device() {
-    return 'ontouchstart' in window // works on most browsers
-        || 'onmsgesturechange' in window; // works on ie10
-  }
-
-  jQuery(document).ready(function() {
-    jQuery('.toggle-nav').click(function(e) {
-        jQuery(this).toggleClass('active');
-        jQuery('.menu ul').toggleClass('active');
-
-        e.preventDefault();
     });
+  }
+}
+
+function is_touch_device() {
+  return 'ontouchstart' in window // works on most browsers
+    || 'onmsgesturechange' in window; // works on ie10
+}
+
+jQuery(document).ready(function() {
+  jQuery('.toggle-nav').click(function(e) {
+    jQuery(this).toggleClass('active');
+    jQuery('.menu ul').toggleClass('active');
+
+    e.preventDefault();
+  });
 });
+
+$(function() {
+    var nav = $('nav li a');
+    console.log(nav)
+    var heightThreshold = $("#contact").offset().top;
+    console.log(heightThreshold)
+    var heightThresholdEnd = $("#contact").offset().top +$("#contact").height() ;
+    console.log(heightThresholdEnd)
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= heightThreshold && scroll <=  heightThresholdEnd ) {
+            nav.addClass('dark');
+        } else {
+            nav.removeClass('dark');
+        }
+    });
+})
